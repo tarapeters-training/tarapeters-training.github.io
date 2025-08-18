4f477414ab18dec17f97f9f8c7e9386e1a0156db
Tara Peters <tarapeters.training@gmail.com>
2025-08-18 13:00:44 -0700

    update more files

---
+++ visualboundariesBranchingStoryline/story_content/6I6A83eX83a_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6I6A83eX83a_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.102.35072.0%0D%0A%0D%0A00:00:00.288%20--%3E%2000:00:01.600%0D%0A(Leah)%20I%20can\'t%20wait%20to%20go%20outside%20to%20play!%0D%0A%0D%0A"}]})
---
+++ visualboundariesBranchingStoryline/story_content/6aHYG48yDyu_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6aHYG48yDyu_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.102.35072.0%0D%0A%0D%0A00:00:00.096%20--%3E%2000:00:01.675%0D%0A%08(Matthew)%20I%20love%20cutting%20with%20scissors!%0D%0A%0D%0A"}]})
