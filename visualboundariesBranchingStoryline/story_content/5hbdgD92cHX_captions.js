a0f3b566cec8dcb1ca554182bbf18c1399a479e9
Tara Peters <tarapeters.training@gmail.com>
2025-08-18 12:59:53 -0700

    delete and re-add folder

---
+++ visualboundariesBranchingStoryline/story_content/5hbdgD92cHX_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5hbdgD92cHX_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.102.35072.0%0D%0A%0D%0A00:00:00.096%20--%3E%2000:00:02.038%0D%0A(Matthew)%20I\'m%20going%20to%20make%20a%20robot%20today!%0D%0A%0D%0A"}]})
---
+++ visualboundariesBranchingStoryline/story_content/5zaHoQjIqUt_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5zaHoQjIqUt_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.102.35072.0%0D%0A%0D%0A00:00:00.288%20--%3E%2000:00:01.600%0D%0A(Leah)%20Stop%20bumping%20into%20me!%0D%0A%0D%0A"}]})
